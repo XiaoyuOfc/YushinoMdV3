0e009f177e8393d87ee1ca5b516d5f0f21b8b48e
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-11-08 14:40:18 +0700

    Update own-addprem.js

--- plugins/own-addprem.js
+++ plugins/own-addprem.js
@@ -1,23 +1,22 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text }) => {
+
     let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    let user = db.data.users[who]
-    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
-    let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `berapa hari mamaskuh?`
-    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
-    var jumlahHari = 86400000 * txt
-    var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-    user.premium = true
-    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium ${txt} hari.`)
+    if (!who) throw `tag orangnya!`
+    if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
+    global.prems.push(`${who.split`@`[0]}`)
+    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
+        contextInfo: {
+            mentionedJid: [who]
+        }
+    })
+
 }
-handler.help = ['addprem [@user] <hari>']
+handler.help = ['addprem [@user]']
 handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)p(rem)?$/i
+handler.command = /^(add|tambah|\+)prem$/i
 
 handler.rowner = true
 
-export default handler
+module.exports = handler
